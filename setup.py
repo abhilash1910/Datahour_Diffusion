34c031ea5e7f562fa8b4b891ad8e7a1ba7ba7539
abhilash1910 <debabhi1396@gmail.com>
2022-10-21 18:15:07 +0530

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="diffusion_tf",
+    version="0.1",
+    description="Stable Diffusion in Tensorflow / Keras",
+    author="",
+    author_email="",
+    platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+    url="https://github.com/abhilash1910/Datahour_Diffusion",
+    packages=find_packages(),
+)
